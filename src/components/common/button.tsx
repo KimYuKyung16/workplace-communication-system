05cb487fc083044ddf76db96a930cf0500770453
KimYuKyung <kyg0954@naver.com>
2023-09-16 23:36:11 +0900

    feat: button 컴포넌트 크기 별 설정 추가

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -4,24 +4,27 @@ import { styled } from "styled-components";
 interface ButtonProps {
   text: string;
   large?: boolean;
+  onClick?: () => void;
 }
 
-function Button({ text, large }: ButtonProps) {
+function Button({ text, onClick, large }: ButtonProps) {
   return (
     <>
-      <ButtonDefault large={large}>{text}</ButtonDefault>
+      <ButtonDefault onClick={onClick} $large={large}>
+        {text}
+      </ButtonDefault>
     </>
   );
 }
 
-const ButtonDefault = styled.button<{ large: boolean | undefined }>`
+const ButtonDefault = styled.button<{ $large: boolean | undefined }>`
   width: 100%;
   background-color: #1c3150;
   border-radius: 10px;
   color: white;
   border: none;
   height: 63px;
-  font-size: ${(props) => (props.large ? "2.2rem" : "1.8rem")};
+  font-size: ${(props) => (props.$large ? "2.2rem" : "1.8rem")};
   box-shadow: 10px 10px 40px rgba(15, 34, 182, 0.16);
   cursor: pointer;
 
